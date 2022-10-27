7fc43bf7ed6854a16f331cfc2a68966be6b5152d
Thiliban <thilip2017@gmail.com>
2022-10-27 12:10:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import datas from "./data.json";
 
 function App() {
+  const [searchWord, setSearchWord] = useState("");
+  const [meaning, setMeaning] = useState("");
+  const [obj, setObj] = useState({});
+  const [data, setData] = useState([]);
+  const [mean, setMean] = useState(false);
+
+  const onChange = (event) => {
+    setSearchWord(event.target.value);
+    const newData = datas.filter(
+      (data) =>
+        event.target.value &&
+        data.name.toLowerCase().startsWith(event.target.value.toLowerCase())
+    );
+    console.log(newData);
+    setData(newData);
+    setMean(false);
+    setMeaning("");
+  };
+
+  const onSearch = (dataObj) => {
+    setSearchWord(dataObj.name);
+    console.log("search", dataObj);
+    setData([]);
+    setObj(dataObj);
+  };
+
+  const setGo = () => {
+    setMean(true);
+    setMeaning(obj.meaning);
+  };
+
+  console.log(searchWord);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container min-vh-100 py-4">
+        <div className="row mt-5">
+          <div className="col-md-5 mx-auto">
+            <div className="fs-2 fw-bold text-primary text-center pb-3">
+              Dev<span className="text-warning">Go</span>
+            </div>
+            <div className="input-group mb-1">
+              <input
+                type="text"
+                className="form-control bg-transparent"
+                placeholder="Search"
+                value={searchWord}
+                onChange={onChange}
+              />
+              <button
+                className="btn btn-dark"
+                onClick={() => {
+                  setGo();
+                }}
+              >
+                Go
+              </button>
+            </div>
+            <div className="dropdown-content mt-0 bg-white p-1 rounded">
+              {data.map((dat, idx) => {
+                return (
+                  <div className="list" onClick={() => onSearch(dat)} key={idx}>
+                    {dat.name}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div>
+          <p style={{ textAlign: "center", marginTop: "3rem" }}>
+            {mean && <p style={{ fontWeight: "700" }}>Meaning</p>}
+            {meaning}
+          </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
